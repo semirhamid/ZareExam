78c48b2a25bd01f3291a03796963ccb4230250fd
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-05-08 17:08:02 +0300

    Database migration history updated

--- Infrastructure/Migrations/Auth/20240508125942_Modified Student Table Relation UserId Data Tpe.Designer.cs
+++ Infrastructure/Migrations/Auth/20240508125942_Modified Student Table Relation UserId Data Tpe.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZareExam.Infrastructure.Db;
 
@@ -11,9 +12,11 @@ using ZareExam.Infrastructure.Db;
 namespace ZareExam.Infrastructure.Migrations.Auth
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240508125942_Modified Student Table Relation UserId Data Tpe")]
+    partial class ModifiedStudentTableRelationUserIdDataTpe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/Auth/20240508125942_Modified Student Table Relation UserId Data Tpe.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZareExam.Infrastructure.Migrations.Auth
+{
+    /// <inheritdoc />
+    public partial class ModifiedStudentTableRelationUserIdDataTpe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_AspNetUsers_UserId1",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_UserId1",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Students",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_UserId",
+                table: "Students",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_AspNetUsers_UserId",
+                table: "Students",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_AspNetUsers_UserId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_UserId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Students",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_UserId1",
+                table: "Students",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_AspNetUsers_UserId1",
+                table: "Students",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
