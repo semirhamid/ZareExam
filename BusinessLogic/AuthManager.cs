36a357389e4629b87db1ca69eda7bf599f5aa513
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-05-08 17:06:47 +0300

    Auth manager updated with token

--- BusinessLogic/AuthManager.cs
+++ BusinessLogic/AuthManager.cs
@@ -225,7 +225,7 @@ namespace ZareExam.BusinessLogic
         private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             // Unix timestamp is seconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToUniversalTime();
             return dtDateTime;
         }
